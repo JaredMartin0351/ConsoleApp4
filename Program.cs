65436ed62759271c993bd5a01d7cb7fbfd765588
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-03-17 14:21:19 -0500

    Feat: learning number functions in C#

--- Program.cs
+++ Program.cs
@@ -40,6 +40,32 @@ namespace ConsoleApp4
             Console.WriteLine(myPhrase.Substring(6, 4));*/
             /*testing dot notation properties for strings*/
 
+            /*int firstNum = 1;
+            int secondNum = 2;
+            int thirdNum = 3;
+            int fourthNum = 37;
+            int fifthNum = 64;
+            firstNum++;
+            firstNum--;*/
+
+
+            /*Console.WriteLine(0351);
+            Console.WriteLine(firstNum);
+            Console.WriteLine(secondNum);
+            Console.WriteLine(thirdNum);
+            Console.WriteLine(firstNum + "\n" + secondNum + "\n" + thirdNum);
+            Console.WriteLine(secondNum * thirdNum);
+            Console.WriteLine(6 / 2);
+            Console.WriteLine(25 % 78);
+            Console.WriteLine(6 + 10 * 2);
+            Console.WriteLine((6 + 10) * 2);
+            Console.WriteLine(10.2 + 3.5);
+            Console.WriteLine(fifthNum % fourthNum);
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(1, 2));
+            Console.WriteLine(Math.Min(3, 10));*/
+            /*learning numbers in C#*/
 
 
             Console.ReadLine();
