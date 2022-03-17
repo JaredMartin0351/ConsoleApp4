830a425cf2352ffb8154c2be26f03c56e1faf746
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-03-17 13:59:37 -0500

    Feat: Learning dot notation properties of strings C#

--- Program.cs
+++ Program.cs
@@ -10,23 +10,33 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            string phrase = "Jared\nMartin";
+            /*string phrase = "Jared\nMartin";
             string anotherPhrase = "jared\"martin";
             char grade = 'A';
             int age = 35;
             double gpa = 4.0;
             bool isMale = true;
-            string yetAnotherPhrase = phrase + "\nis cool\n" + anotherPhrase;
-            // Learning C# concatination, variables, working with strings
+            string yetAnotherPhrase = phrase + "\nis cool\n" + anotherPhrase;*/
+            /*Learning C# concatination, variables, working with strings*/
 
-            Console.WriteLine(phrase);
+            /*Console.WriteLine(phrase);
             Console.WriteLine(age);
             Console.WriteLine(gpa);
             Console.WriteLine(grade);
             Console.WriteLine(isMale);
             Console.WriteLine(anotherPhrase);
-            Console.WriteLine(yetAnotherPhrase);    
-            
+            Console.WriteLine(yetAnotherPhrase);*/
+
+            /*string myPhrase = "Jared Martin";
+            Console.WriteLine(myPhrase.ToUpper());
+            Console.WriteLine(myPhrase.ToLower());
+            Console.WriteLine(myPhrase.Contains("Jared"));
+            Console.WriteLine(myPhrase[0]);
+            Console.WriteLine(myPhrase[1]);
+            Console.WriteLine(myPhrase[2]);
+            Console.WriteLine(myPhrase[3]);
+            Console.WriteLine(myPhrase[4]);*/
+            /*testing dot notation properties for strings*/
 
 
 
